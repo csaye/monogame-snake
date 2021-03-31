a27dc276739ccfe7e24e2641492c6bb543e03809
Cooper Saye <coopersaye@gmail.com>
2021-03-30 19:56:21 -0600

    Implement snake.

--- Snake/Game1.cs
+++ Snake/Game1.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Snake
 {
     public class Game1 : Game
     {
+        // Monogame declarations
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
+
+        private enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        }
 
         private enum BlockType
         {
@@ -21,6 +31,21 @@ namespace Snake
         private const int BoardWidth = 16;
         private const int BoardHeight = 16;
         private BlockType[,] board = new BlockType[BoardWidth, BoardHeight];
+
+        // Snake
+        private struct Snake
+        {
+            public Snake(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        Snake snake = new Snake(0, 0);
 
         public Game1()
         {
@@ -53,6 +78,16 @@ namespace Snake
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+
+            Texture2D snakeTexture = new Texture2D(GraphicsDevice, 1, 1);
+            snakeTexture.SetData(new Color[] { Color.Green });
+            Rectangle snakeRect = new Rectangle(snake.X * GridSize, snake.Y * GridSize, GridSize, GridSize);
+            spriteBatch.Draw(snakeTexture, snakeRect, Color.White);
+
+            spriteBatch.End();
+
             base.Draw(gameTime);
         }
     }
